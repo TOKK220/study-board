688337434cba069b42977f4e5c78989e542dc4a7
tsyrul-alexander <tsyrulalexander@gmail.com>
2021-03-17 17:37:51 +0200

    [#2] Create domain base classes Create auth controllers

---
+++ StudyBoard.Auth.WebApi/Controller/AuthenticationController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StudyBoard.Core.Web.Controller;
+
+namespace StudyBoard.Auth.WebApi.Controller {
+	[Route("authentication")]
+	public class AuthenticationBaseController : BaseController {
+		public AuthenticationBaseController(ILogger logger) : base(logger) { }
+	}
+}
---
+++ StudyBoard.Auth.WebApi/Controller/AuthorizationController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StudyBoard.Core.Web.Controller;
+
+namespace StudyBoard.Auth.WebApi.Controller {
+	[Route("authorization")]
+	public class AuthorizationController : BaseController {
+		public AuthorizationController(ILogger logger) : base(logger) { }
+	}
+}
