25ef4aa2a557ff58b4a895a2d0645058ebea4035
tsyrul-alexander <tsyrulalexander@gmail.com>
2021-03-21 22:36:38 +0200

    [#2] Create domain base classes create settings class

--- StudyBoard.Auth.WebApi/Startup.cs
+++ StudyBoard.Auth.WebApi/Startup.cs
@@ -5,36 +5,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace StudyBoard.Auth.WebApi
-{
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
-        }
-    }
+namespace StudyBoard.Auth.WebApi {
+	public class Startup {
+		private readonly AuthSettings _settings;
+		public Startup(IConfiguration configuration) {
+			_settings = new AuthSettings(configuration);
+		}
+		public void ConfigureServices(IServiceCollection services) {
+			services.AddSingleton(_settings);
+			var conn = _settings.DataBaseConnectionString;
+		}
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+			if (env.IsDevelopment()) {
+				app.UseDeveloperExceptionPage();
+			}
+			app.UseRouting();
+			app.UseEndpoints(endpoints => {
+				endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+			});
+		}
+	}
 }
