9f6f1cf92ce9bad7d5c43e734b62ebf7cf069fe6
tsyrul-alexander <tsyrulalexander@gmail.com>
2021-03-22 21:20:18 +0200

    [#2] Create domain base classes Create contact request and response classes

--- StudyBoard.Auth.WebApi/Controller/AuthorizationController.cs
+++ StudyBoard.Auth.WebApi/Controller/AuthorizationController.cs
@@ -2,12 +2,14 @@
 using Microsoft.Extensions.Logging;
 using StudyBoard.Core.Web.Controller;
 
-namespace StudyBoard.Auth.WebApi.Controller {
+namespace StudyBoard.Auth.WebApi.Controller
+{
 	/// <summary>
 	/// Controller for granting a certain person the rights to perform certain actions.
 	/// </summary>
 	[Route("authorization")]
-	public class AuthorizationController : BaseController {
+	public class AuthorizationController : BaseController
+	{
 		public AuthorizationController(ILogger logger) : base(logger) { }
 	}
 }
---
+++ StudyBoard.DataFacade.WebApi/Model/CreateContactRequest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudyBoard.DataFacade.WebApi.Model
+{
+	public class CreateContactRequest
+	{
+		[BindProperty(Name = "firstName")]
+		public string FirstName { get; set; }
+		[BindProperty(Name = "lastName")]
+		public string LastName { get; set; }
+		[BindProperty(Name = "phoneNumber")]
+		public string PhoneNumber { get; set; }
+		[BindProperty(Name = "email")]
+		public string Email { get; set; }
+	}
+}
