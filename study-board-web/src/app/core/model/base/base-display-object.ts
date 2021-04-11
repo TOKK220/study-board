49b416ef837f225ef869102f247c5a26b0679c6e
tsyrul-alexander <tsyrulalexander@gmail.com>
2021-04-11 11:08:03 +0000

    [#5] Base classes

---
+++ study-board-web/src/app/core/model/base/base-display-object.ts
@@ -0,0 +1,9 @@
+import {Guid} from "guid-typescript";
+import { BaseObject } from "./base-object";
+
+export class BaseDisplayObject extends BaseObject {
+	constructor(id: Guid = null, public name: string = null) {
+		super(id);
+	}
+
+}
