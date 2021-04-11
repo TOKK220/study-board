4487b57bf0b7f814795c402c642a98607aa710b8
tsyrul-alexander <tsyrulalexander@gmail.com>
2021-04-11 11:07:45 +0000

    [#5] Route to registration component

--- study-board-web/src/app/auth/auth.module.ts
+++ study-board-web/src/app/auth/auth.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AuthRoutingModule } from './auth-routing.module';
+import { RegistrationComponent } from './controllers/registration/registration.component';
+import { SharedModule } from '@shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegistrationComponent
+  ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    SharedModule
   ]
 })
 export class AuthModule { }
