50369ad91ec7194782ef37d837065f8836dc921a
tsyrul-alexander <tsyrulalexander@gmail.com>
2021-04-13 08:42:07 +0000

    [#5] move reqistration model

--- study-board-web/src/app/auth/controllers/registration/registration.component.ts
+++ study-board-web/src/app/auth/controllers/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Registration } from '@core/auth/registration';
+import { Registration } from '@core/model/auth/registration';
 import { Guid } from 'guid-typescript';
 
 @Component({
