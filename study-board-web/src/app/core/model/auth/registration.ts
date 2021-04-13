316cdddd63ead4e1cace2237c20b827637eb1fa1
tsyrul-alexander <tsyrulalexander@gmail.com>
2021-04-13 10:56:57 +0000

    [#5] add fields to registration

--- study-board-web/src/app/core/model/auth/registration.ts
+++ study-board-web/src/app/core/model/auth/registration.ts
@@ -4,4 +4,8 @@ export class Registration extends BaseObject {
     public name: string;
 	public login: string;
 	public password: string;
+	public firstName: string;
+	public lastName: string;
+	public phoneNumber: string;
+	public email: string;
 }
