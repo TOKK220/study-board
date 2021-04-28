1bb70c334974df4a035cdcc327d73975ea9805d9
tsyrul-alexander <tsyrulalexander@gmail.com>
2021-04-28 15:40:03 +0000

    [#5] isSession to global property

--- study-board-web/src/app/core/service/auth.service.ts
+++ study-board-web/src/app/core/service/auth.service.ts
@@ -7,29 +7,45 @@ import { environment } from "environment/environment";
 import { Observable } from "rxjs";
 import { BaseHttpService } from "./base-http.service";
 
-@Injectable()
+@Injectable({
+	providedIn: "root"
+})
 export class AuthService extends BaseHttpService {
-    private url = "/api/auth";
-    public AuthorizedStorageKey: string  = environment.authTokenStorageKey;
-
+	private url = "/api/auth";
+	public authorizedStorageKey: string = environment.authTokenStorageKey;
+	public isSession: boolean = false;
 	public login(request: LoginRequest): Observable<LoginResponse> {
 		return this.castObject(this.http.post(this.url + "/authentication/login", request), LoginResponse);
 	}
-    public registration(request: RegistrationRequest): Observable<RegistrationResponse> {
-        return this.castObject(this.http.post(this.url + "/authentication/registration", request), RegistrationResponse);
-    }
-    public isLoggedIn(): boolean {
-        return !!this.getToken();
-    }
-    public getToken() : string {
-		return sessionStorage.getItem(this.AuthorizedStorageKey) ||
-			localStorage.getItem(this.AuthorizedStorageKey);
+	public registration(request: RegistrationRequest): Observable<RegistrationResponse> {
+		return this.castObject(this.http.post(this.url + "/authentication/registration", request), RegistrationResponse);
 	}
-	public setToken(token: string, isSession: boolean = false) {
-		if (isSession) {
-			sessionStorage.setItem(this.AuthorizedStorageKey, token);
-		} else {
-			localStorage.setItem(this.AuthorizedStorageKey, token);
+	public isLoggedIn(): boolean {
+		return !!this.getToken();
+	}
+	public getToken(): string {
+		if (this.isSession) {
+			return this.getSessionToken();
 		}
+		return this.getLocalToken();
+	}
+	public setToken(token: string) {
+		if (this.isSession) {
+			this.setSessionToken(token);
+		} else {
+			this.setLocalToken(token);
+		}
+	}
+	protected setSessionToken(token: string): void {
+		sessionStorage.setItem(this.authorizedStorageKey, token);
+	}
+	protected setLocalToken(token: string): void {
+		localStorage.setItem(this.authorizedStorageKey, token);
+	}
+	protected getSessionToken(): string {
+		return sessionStorage.getItem(this.authorizedStorageKey);
+	}
+	protected getLocalToken(): string {
+		return localStorage.getItem(this.authorizedStorageKey);
 	}
 }
